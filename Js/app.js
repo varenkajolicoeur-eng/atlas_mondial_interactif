1e7b854e147c8003c5585223ac2399a177fdede5
Varenka Jolicoeur <varenkajolicoeur@gmail.com>
2026-07-13 10:05:58 -0400

    Ajout de la gestion de l'evenement submit du formulaire

--- Js/app.js
+++ Js/app.js
@@ -7,6 +7,15 @@ const messageErreur = document.getElementById("message-erreur");
 const messageChargement = document.getElementById("message-chargement");
 const resultats = document.getElementById("resultats");
 
+/*====================================
+    ÉCOUTER LE FORMULAIRE
+====================================*/
+formRecherche.addEventListener("submit", async function(event){
+
+    event.preventDefault();
+    
+});
+
 
 
 
