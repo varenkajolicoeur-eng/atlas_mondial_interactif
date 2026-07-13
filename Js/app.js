ab856ed4c8e6355b80740b2f230c0864dd4a26d1
Varenka Jolicoeur <varenkajolicoeur@gmail.com>
2026-07-13 14:58:42 -0400

    Affichage des resultats avec styles css

--- Js/app.js
+++ Js/app.js
@@ -19,6 +19,9 @@ formRecherche.addEventListener("submit", async function(event){
         return;
     }
     messageErreur.textContent = "";
+    /*====================================
+    Appel de l'API REST Countries
+    ====================================*/
     try{
         messageChargement.classList.remove("hidden");
          const reponse = await fetch(
@@ -31,9 +34,67 @@ formRecherche.addEventListener("submit", async function(event){
         );
         const donnees = await reponse.json();
         messageChargement.classList.add("hidden");
-        console.log(donnees);
-        const paystrouve = donnees.data.objects[0];
-        console.log(paystrouve)
+        const paysTrouve = donnees.data.objects[0];
+        
+        // Affichage des résultats
+
+        resultats.replaceChildren();
+        // Création de la carte du pays
+        const cartePays = document.createElement("article");
+        cartePays.classList.add("carte-pays");
+
+        //creation d'une fonction pour creer les elements HTML pour chaque information du pays.
+        function creerLigne(libelle, valeur) {
+
+            const paragraphe = document.createElement("p");
+
+            const etiquette = document.createElement("strong");
+            etiquette.textContent = `${libelle} :`;
+
+            const contenu = document.createElement("span");
+            contenu.textContent = valeur;
+
+            paragraphe.appendChild(etiquette);
+            paragraphe.append(" ");
+            paragraphe.appendChild(contenu);
+
+            return paragraphe;
+        }
+        // Création des éléments HTML pour chaque information du pays.
+        const titre = document.createElement("h2");
+        titre.textContent = paysTrouve.names.common;
+        cartePays.appendChild(titre);
+        
+        const drapeauPays = document.createElement("div");
+        drapeauPays.classList.add("drapeau-pays");
+
+        const infosPays = document.createElement("div");
+        infosPays.classList.add("infos-pays");
+
+        
+
+        const drapeau = document.createElement("img");
+        drapeau.classList.add("drapeau");
+        drapeau.src = paysTrouve.flag.url_svg;
+        drapeau.alt = `Drapeau de ${paysTrouve.names.common}`;
+        drapeauPays.appendChild(drapeau);
+        cartePays.appendChild(drapeauPays);
+
+        infosPays.appendChild(creerLigne("Nom officiel", paysTrouve.names.official));
+
+        infosPays.appendChild(creerLigne("Capitale", paysTrouve.capitals[0].name));
+        
+        infosPays.appendChild(creerLigne("Population", paysTrouve.population.toLocaleString()));
+
+        infosPays.appendChild(creerLigne("Région", paysTrouve.region));
+
+        infosPays.appendChild(creerLigne("Monnaie", `${paysTrouve.currencies[0].name} (${paysTrouve.currencies[0].symbol})`));
+
+        infosPays.appendChild(creerLigne("Langues", `${paysTrouve.languages.map(langue => langue.name).join(", ")}`));
+
+        cartePays.appendChild(infosPays);
+        resultats.appendChild(cartePays);
+        
     }
      catch(error){
         messageChargement.classList.add("hidden");
