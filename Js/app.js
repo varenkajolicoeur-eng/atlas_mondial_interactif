d032ed210b08283443a5b53b0dd931df6a7ba124
Varenka Jolicoeur <varenkajolicoeur@gmail.com>
2026-07-13 10:11:21 -0400

    Integration de l'API Rest Countries

--- Js/app.js
+++ Js/app.js
@@ -19,6 +19,28 @@ formRecherche.addEventListener("submit", async function(event){
         return;
     }
     messageErreur.textContent = "";
+    try{
+        messageChargement.classList.remove("hidden");
+         const reponse = await fetch(
+            `https://api.restcountries.com/countries/v5?q=${pays}`,
+        { 
+            headers: { 
+                Authorization: 'Bearer rc_live_b9a348982d8e47b798230b53efe80b95'
+            }
+        }
+        );
+        const donnees = await reponse.json();
+        messageChargement.classList.add("hidden");
+        console.log(donnees);
+        const paystrouve = donnees.data.objects[0];
+        console.log(paystrouve)
+    }
+     catch(error){
+        messageChargement.classList.add("hidden");
+
+        messageErreur.textContent = "Une erreur est survenu lors de la recherche.";
+
+     }
     
    
     
