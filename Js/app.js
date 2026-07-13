31d01fd3b302f2f45b2fc92c21d8a61af6db8809
Varenka Jolicoeur <varenkajolicoeur@gmail.com>
2026-07-13 09:49:15 -0400

    Initialisation de la structure JavaScript et recuperation des elements du DOM

---
+++ Js/app.js
@@ -0,0 +1,12 @@
+/*================================================
+=           Recuperation des elements HTML        
+=================================================*/
+const formRecherche = document.getElementById("form-recherche");
+const inputPays = document.getElementById("pays");
+const messageErreur = document.getElementById("message-erreur");
+const messageChargement = document.getElementById("message-chargement");
+const resultats = document.getElementById("resultats");
+
+
+
+
